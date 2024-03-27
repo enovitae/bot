324fb453df52c935c68a9e32f2398928aad90cfe
Alessandro Sebastiani <sebbalex@gmail.com>
2024-03-27 15:30:21 +0100

    chore: readdir

--- src/poller.ts
+++ src/poller.ts
@@ -1,7 +1,10 @@
-import { readFileSync } from 'fs'
+import { readFileSync, readdirSync } from 'fs'
 import { CODE_PATH } from './config'
 
 export const checkFSAccess = (): boolean => {
+  const dir = readdirSync(`${CODE_PATH}`, 'utf8')
+  console.log(JSON.stringify(dir))
+
   const src = readFileSync(`${CODE_PATH}/global.css`, 'utf8')
   console.log('trying to read a sample project file', src)
   return !!src
